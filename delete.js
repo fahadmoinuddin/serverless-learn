ddfe9855f43be6d0d60e05dbc783aac191a4334f
kalanithy <fahadmoinuddin@yahoo.com>
2018-07-26 13:01:39 +0530

    First commit

---
+++ delete.js
@@ -0,0 +1,19 @@
+import * as dynamoDbLib from "./libs/dynamo-lib.js";
+import { success, failure } from "./libs/response-lib.js";
+
+export async function main(event, context, callback) {
+	const params = {
+		TableName: "notes",
+		Key: {
+			userId: event.requestContext.identity.cognitoIdentityId,
+			noteId: event.pathParameters.id
+		}
+	};
+
+	try{
+		const result = await dynamoDbLib.call("delete", params);
+		callback(null, success({status: true}));
+	}catch(e){
+		callback(null, failure({status: false}));
+	}
+}
